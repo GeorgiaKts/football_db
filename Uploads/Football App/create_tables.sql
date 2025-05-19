41f0a301b91ae2435ee9db95c66d0fd1d64e429b
Ioanna Kalpourtzi <ikalpourtzi@users.noreply.github.com>
2025-05-19 22:02:30 +0300

    Delete Uploads directory

--- Uploads/Football App/create_tables.sql
+++
@@ -1,9 +0,0 @@
------------------------------------ Load file's data countries	
-USE football_db;
-
-LOAD DATA INFILE 'C:\Program Files\MySQL\MySQL Server 8.0\Uploads\cleaned_countries.csv'
-INTO TABLE countries
-FIELDS TERMINATED BY '\t'
-ENCLOSED BY '"'
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES;
